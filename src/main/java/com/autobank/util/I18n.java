7fb1a60074fb1f0405a29a6cfefb4dede388df02
shree <shreerajgudade124@gmail.com>
2026-05-27 21:46:06 +0530

    marathi

--- src/main/java/com/autobank/util/I18n.java
+++ src/main/java/com/autobank/util/I18n.java
@@ -5,7 +5,10 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 public class I18n {
 
@@ -28,4 +31,18 @@ public class I18n {
     }
 
     public static String getCurrentLang() { return currentLang; }
+
+    public static ResourceBundle getBundle() {
+        return new ResourceBundle() {
+            @Override
+            protected Object handleGetObject(String key) {
+                return strings.get(key);
+            }
+
+            @Override
+            public Enumeration<String> getKeys() {
+                return Collections.enumeration(strings.keySet());
+            }
+        };
+    }
 }

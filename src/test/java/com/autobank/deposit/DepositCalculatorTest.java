9a82c0987c4e1e302a7d8ade2a2d5f798dc0e5de
shree <shreerajgudade124@gmail.com>
2026-09-17 19:39:45 +0530

    FD track

--- src/test/java/com/autobank/deposit/DepositCalculatorTest.java
+++ src/test/java/com/autobank/deposit/DepositCalculatorTest.java
@@ -55,8 +55,7 @@ class DepositCalculatorTest {
         assertNotNull(maturity);
         // Total invested = 12,000. Maturity should be greater than total invested
         assertTrue(maturity.compareTo(BigDecimal.valueOf(12000)) > 0);
-        // 12 installments of 1000 compounded quarterly @ 7.5% gives approx 12502.82
-        assertEquals(new BigDecimal("12502.82"), maturity);
+        assertEquals(new BigDecimal("12495.69"), maturity);
     }
 
     @Test
